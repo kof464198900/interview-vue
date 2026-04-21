9aebcd43ef440128d3ddab068465428aa54271c4
kofkk <464198900@qq.com>
2026-04-21 19:30:08 +0800

    feat: 开启Vite代理支持手机局域网访问

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        // target: 'http://localhost:8080',
-         //target: 'https://java-interview-backend-production.up.railway.app',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
